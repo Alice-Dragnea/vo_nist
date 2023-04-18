e7fd9c157b8dd3fe6e400671856e1e0ff89cddc9
cloeb <cloeb@purdue.edu>
2023-04-17 22:11:43 -0400

    fixed entry point

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'odom = vo_nist:main',
+            'odom = vo_nist.run:main',
         ],
     },
 )
